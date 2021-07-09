0523abbb3669065b233b34fafd15378834d69a95
Min Chang <minchangmhc@gmail.com>
2021-07-09 09:43:21 -0700

    Add failing test for actions.

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -1,4 +1,11 @@
 import * as actions from './../../actions'
 import * as c from './../../actions/ActionTypes';
 
-describe()
+describe('tap room actions', () => {
+  it('deleteDrink should create DELETE_DRINK action', () => {
+    expect(actions.deleteDrink(1)).toEqual({
+      type: c.DELETE_DRINK,
+      id: 1
+    })
+  })
+})
